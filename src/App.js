e261c251afbd1b54cf93513bd9667994f63d0fef
hm-factor <hsmanel@gmail.com>
2021-10-05 14:25:30 -0400

    initial react setup, removed unecessary boiler plate, added useless search bar

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input class="search-bar" placeholder="Find Homes..."></input>
+    </div>
+  );
+}
+
+export default App;
