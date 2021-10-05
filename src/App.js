51f1ff3bb903217404df46eb2a9377a17e530572
hm-factor <hsmanel@gmail.com>
2021-10-05 17:24:20 -0400

    fetches all homes from database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import axios from 'axios';
+import React, {useState} from 'react';
 
 function App() {
+  let [homes, setHomes] = useState(['empty']);
+
+  const getHomes = async (e) => {
+    await axios.get(`http://localhost:3000/api/search`, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((res)=>setHomes(res.data))
+      .catch((err)=>console.log(err.message))
+  }
+
+  let homesList = Object.keys(homes).map((k)=>{
+    return (
+      <li>
+        {homes[k]['address']}
+      </li>
+    )
+  })
+
   return (
     <div className="App">
-      <input class="search-bar" placeholder="Find Homes..."></input>
+      <input className="search-bar" placeholder="Find Homes..."></input>
+      <input onClick={(e)=>getHomes(e)} type="submit" value="Submit"/>
+      <ul>
+        {homesList}
+      </ul>
     </div>
   );
 }
