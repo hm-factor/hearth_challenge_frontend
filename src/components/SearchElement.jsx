943da372b628491f1206769b7c5cedf0b59081f3
hm-factor <hsmanel@gmail.com>
2021-10-06 19:18:19 -0400

    onclick functionality added to searchelements to expose extra info

--- src/components/SearchElement.jsx
+++ src/components/SearchElement.jsx
@@ -1,11 +1,31 @@
+import '../App.css';
 import React, { useState } from "react";
 
 export default function SearchElement(props) {
-  const { address, zip, baths, beds, city, square_feet, state } = props.home;
+  const [style, setStyle] = useState('off')
+  let { address, zip, baths, beds, city, square_feet, state , price} = props.home;
+
+  const handleClick = () => {
+    (style === 'off') ? setStyle('on') : setStyle('off')
+  }
+
+  const formatPrice = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  price = formatPrice.format(price);
 
   return (
-    <div className="more-info">
+    <div onClick={()=>handleClick()}>
       {address}
+      <div className={`more-info-${style}`}>
+        <div>{price}</div>
+        <div>{baths} bas</div>
+        <div>{beds} bds</div>
+        <div>{square_feet} sqft</div>
+        <div>{city}, {state} {zip}</div>
+      </div>
     </div>
-  )
+  );
 }
