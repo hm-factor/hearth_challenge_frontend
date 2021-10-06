f59c1260597b086d8df24b00f30194b5fa512e19
hm-factor <hsmanel@gmail.com>
2021-10-06 11:56:12 -0400

    properly passing searchParams back to rails and updating searchParams state variables

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import React, {useState} from 'react';
 
 function App() {
   let [homes, setHomes] = useState(['empty']);
+  let [searchParams, setSearchParams] = useState('');
 
   const getHomes = async (e) => {
-    await axios.get(`http://localhost:3000/api/search`, {
+    await axios.get(`http://localhost:3000/api/search?search_params=${searchParams}`, {
       headers: {
         'Content-Type': 'application/json'
       }
     })
       .then((res)=>setHomes(res.data))
       .catch((err)=>console.log(err.message))
+  }
+
+  const updateSearchParams = (lt) => {
+    console.log(lt)
+    setSearchParams(lt)
   }
 
   let homesList = Object.keys(homes).map((k)=>{
@@ -25,7 +31,12 @@ function App() {
 
   return (
     <div className="App">
-      <input className="search-bar" placeholder="Find Homes..."></input>
+      <input 
+        className="search-bar" 
+        placeholder="Find Homes..." 
+        value={searchParams}
+        onChange={(e)=>updateSearchParams(e.target.value)}
+      />
       <input onClick={(e)=>getHomes(e)} type="submit" value="Submit"/>
       <ul>
         {homesList}
